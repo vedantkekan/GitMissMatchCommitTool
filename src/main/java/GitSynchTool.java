6380d509f8cedad5e6a80bf76c1e5ec9bfe8b744
Vedant Kekan <mailbox.vedantkekan@gmail.com>
2018-04-14 11:03:18 +0530

    maven structure created

---
+++ src/main/java/GitSynchTool.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+import org.eclipse.jgit.api.Git;
+
+public class GitSynchTool {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
